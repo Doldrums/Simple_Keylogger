c57f09be5c1ba7b41de37c1af1b4a2281c1c3d46
Doldrums <doldrums.317@gmail.com>
2019-01-26 18:26:24 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using System.Diagnostics;
+//using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // инициализируем параметры
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_KEYUP = 0x0101;
+        private const int WM_SYSKEYUP = 0x0105;
+        private const int WM_SYSKEYDOWN = 0x0104;
+        public const int KF_REPEAT = 0X40000000;
+        private const int VK_SHIFT = 0x10;	// SHIFT
+        private const int VK_CONTROL = 0x11;	// CONTROL
+        private const int VK_MENU = 0x12; // ALT
+        private const int VK_CAPITAL = 0x14; // CAPS LOCK
+
+        //private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        public static string mss;
+        public static int myi = 0;
+
+        [STAThread]
+        static void Main(string[] args) {
+            var handle = GetConsoleWindow();
+        }
+    }
+}
